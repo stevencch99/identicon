41f2829c3f596859b695310b28c8b240c623796a
Steven C <stevencch99@gmail.com>
2021-06-17 01:03:43 +0800

    initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,30 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identicon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
---
+++ lib/image.ex
@@ -0,0 +1,4 @@
+defmodule Identicon.Image do
+  # define with default property hex: nil
+  defstruct hex: nil
+end
