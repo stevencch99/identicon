86cf3ae61e8851fcdd52c8755f2d7b93d0be6faf
Steven C <stevencch99@gmail.com>
2021-06-18 00:54:56 +0800

    Mapping with custom functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,36 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_gride
+  end
+
+  def build_gride(%Identicon.Image{hex: hex} = image) do
+
+    # TODO:
+    # 1. Why using Enum.chunk_every(3) will break?
+    # 2. Why there's some string inside the list when address some certain input
+    # iex(12)> Identicon.main("a")
+    # [
+    #   [12, 193, 117, 193, 12],
+    #   [185, 192, 241, 192, 185],
+    #   [182, 168, 49, 168, 182],
+    #   [195, 153, 226, 153, 195],
+    #   'iw&wi'
+    # ]
+
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+
+  def mirror_row(row) do
+    inspect(row)
+    # [145, 46, 200]
+    [first, second | _tail] = row
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail ]} = image) do
@@ -21,5 +51,13 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  def print_item(item) do
+    IO.puts "PUTS*******************"
+    IO.puts(item)
+    IO.puts "INSPECT****************"
+    inspect(item)
+    IO.puts "END********************"
   end
 end
