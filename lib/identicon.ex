8bdbfa22dfef7f13d34f1a5ece82c60461edc492
Steven C <stevencch99@gmail.com>
2021-06-18 23:21:54 +0800

    Creating the Pixel Map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,21 @@ defmodule Identicon do
     |> pick_color
     |> build_gride
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horixontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horixontal, vertical}
+      bottom_right = {horixontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   @moduledoc """
   A Struct with properties: hex: nil, color: nil, grid: nil
   """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
